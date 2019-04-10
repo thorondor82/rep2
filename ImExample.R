2c82097fb2dc9f5178235e69ed56faae3616be24
Brett Ruether <brettruether@gmail.com>
2019-04-10 15:21:50 -0600

    Final

--- ImExample.R
+++ ImExample.R
@@ -8,3 +8,7 @@ abline(fit, col=2)
 windows()
 dev.set(5)
 plot(x,y)
+
+Co2 = read.csv("co2_mm_mlo.csv")
+plot(Co2$decdate, Co2$interpolated, type = 'l', main = "Mauna Loa Co2", xlab = "Time", ylab = "ppm")
+lines(Co2$decdate, Co2$trend, col=2, lwd=2)
